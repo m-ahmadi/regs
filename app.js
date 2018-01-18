981451f6ea31ad5c8786b0ab17198478efd7a5ba
m-ahmadi <mohammad.ahmadi1989@yahoo.com>
2018-01-18 18:56:32 +0330

    2

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ let favicon = require('serve-favicon');
 let logger = require('morgan');
 let cookieParser = require('cookie-parser');
 let bodyParser = require('body-parser');
+let xhbs  = require('express-handlebars');
 
 let index = require('./routes/index');
 let users = require('./routes/users');
@@ -12,10 +13,10 @@ let app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
+app.engine('handlebars', xhbs({defaultLayout: 'main'}));
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
